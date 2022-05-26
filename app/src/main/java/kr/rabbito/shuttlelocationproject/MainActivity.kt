81efeb37fc3b793fa1f10eb4a1e5ba3d45982fbc
ThinkingDobby <chiho1221@naver.com>
2022-05-26 10:51:19 +0900

    이미지 추가 및 세부 디자인 수정
    <FIN>
    PostActivity 버튼 이미지 변경, 그림자 적용
    앱 아이콘 임시 적용
    앱 이름 임시 적용
    MainActivity 상태바 투명화

--- app/src/main/java/kr/rabbito/shuttlelocationproject/MainActivity.kt
+++ app/src/main/java/kr/rabbito/shuttlelocationproject/MainActivity.kt
@@ -1,7 +1,11 @@
 package kr.rabbito.shuttlelocationproject
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,11 +25,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         private lateinit var map: GoogleMap
         private val postList = mutableListOf<Location>()
 
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             mBinding = ActivityMainBinding.inflate(layoutInflater)
 
             setContentView(binding.root)
+
+            window.apply {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                statusBarColor = Color.TRANSPARENT
+            }
 
             val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
